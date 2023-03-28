ec4edf934c710023575f96515859c1b1baed3bb8
Flowhero123 <124709512+Flowhero123@users.noreply.github.com>
2023-03-28 13:55:16 -0400

    adding files

---
+++ Assets/Scenes/code/DoorOpen.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = GameObject.Find("Player").GetComponent<PlayerControl>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnMouseDown()
+    {
+         if(playerScript.hasKey ==true)
+        {
+            Destroy(gameObject);
+
+
+        }
+
+
+
+    }
+
+
+
+
+}
